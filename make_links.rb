f2a1c74045c23d2371716be772869e6ebd67958b
Zuyang Kou <leafduo@gmail.com>
2012-06-18 02:46:10 +0800

    Add make links script.

---
+++ make_links.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+# from http://errtheblog.com/posts/89-huba-huba
+
+home = File.expand_path('~')
+
+Dir['.*'].each do |file|
+    next if file =~ /^(.git|.|..)$/
+    target = File.join("dotfiles", "#{file}")
+    `cd ~ && ln -s #{target} #{file}`
+end
+

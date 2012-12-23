09a3c0867f63b06c3ea168f60f157b01f27a291c
leafduo <leafduo@gmail.com>
2012-12-23 22:02:14 +0800

    Define GOROOT only on OS X.

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -4,7 +4,9 @@ export EDITOR="vim"
 export SVN_EDITOR="vim"
 export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/site-packages
 
-export GOROOT=`brew --cellar`/go/HEAD
+if [[ $(uname) == 'Darwin' ]] then
+    export GOROOT=`brew --cellar`/go/HEAD
+fi
 export GOBIN=/usr/local/bin
 export GOARCH=amd64
 export GOOS=darwin
